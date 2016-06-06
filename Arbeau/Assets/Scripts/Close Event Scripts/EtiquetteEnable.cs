4cfc44a4eaef8f31ced2a7a46dccf5d7c3a591c0
Dylan Ilvento <dylanilvento@gmail.com>
2016-06-06 14:49:28 -0400

    Finished Mark IX Window.

---
+++ Arbeau/Assets/Scripts/Close Event Scripts/EtiquetteEnable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EtiquetteEnable : CloseEvent {
+	
+	public GameObject ettiquetteWindow;
+
+	public override void StartEvent () {
+		
+		GameObject win;
+
+		win = (GameObject) Instantiate (ettiquetteWindow, new Vector2(0f, 0f), transform.rotation);
+
+		win.name = "Arbeau Pop-up Window";
+		win.transform.SetParent(GameObject.Find("Canvas").transform, false);
+
+	}
+}
